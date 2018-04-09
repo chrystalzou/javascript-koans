a40d64bc817cb3a2d70025068745c0e24c7bb6f8
Chrystal Zou <chrystalzou@gmail.com>
2018-04-09 11:41:06 +0800

    Modified a typo on line 59 in koans/AboutApplyingWhatWeHaveLearnt.js

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -56,7 +56,7 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(23316);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
